c8135c51fae4144df2373b695c04ed5c8fc3bb1b
mrchoiRyzen7 <drazilletoledo@gmail.com>
2025-03-03 23:20:41 +0800

    Updated

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 window.onload; {
   let element = document.getElementById("modalContainer");
     element.style.display = "none";
+  let logoutBtn = document.getElementById("logoutBtn");
+  logoutBtn.style.display= "none";
 }
 
 function openLogin() {
     //document.getElementById("navBar").style.backgroundColor = "red";
 
     let element = document.getElementById("modalContainer");
+    //let accOptTab = document.getElementById("accOpt");
+    //accOptTab.hide.bs.collapse;
+    let accNavBarElement = document.getElementById("accNavBarBtn");
+    accNavBarElement.hasAttribute("disabled");
     element.style.display = "flex";
     element.style.transition= "1.0s";
+    console.log(accNavBarElement.attributes);
 };
 
 function closeLogin(){
   let element = document.getElementById("modalContainer");
+  let accNavBarElement = document.getElementById("accNavBarBtn");
+  accNavBarElement.removeAttribute("disabled");
   element.style.display = "none";
+  console.log(accNavBarElement.attributes);
 }
